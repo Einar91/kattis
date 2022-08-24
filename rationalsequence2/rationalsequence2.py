aa157c1326178e66dc720bce302581f0706af168
Einar K <50133389+Einar91@users.noreply.github.com>
2022-08-24 18:13:44 +0200

    Accepted solution

--- rationalsequence2/rationalsequence2.py
+++ rationalsequence2/rationalsequence2.py
@@ -1,12 +1,4 @@
-def find_n(p, q):
-    tree = [(1, 1)]
-    i = 0
-    while tree[i] != (p, q):
-        np, nq = tree[i]
-        tree.append((np, (np+nq)))
-        tree.append(((np+nq), nq))
-        i += 1
-    return i+1
+from re import I
 
 
 datasets = int(input())
@@ -14,6 +6,24 @@ for _ in range(datasets):
     line = input().split()
     k = int(line[0])
     p, q = line[1].split("/")
+    p = int(p)
+    q = int(q)
 
-    n = find_n(int(p), int(q))
-    print(k, n)
+    path = ""
+    while p > 1 or q > 1:
+        if (q > p):
+            q = q-p
+            path += "L"
+        else:
+            p = p-q
+            path += "R"
+
+    rev_path = list(path)
+    rev_path.reverse()
+    i = 1
+    print(rev_path)
+    for e in rev_path:
+        i = 2*i
+        if e == "R":
+            i += 1
+    print(k, i)
